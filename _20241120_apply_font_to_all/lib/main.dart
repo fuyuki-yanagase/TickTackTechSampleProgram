173b4deeb9f496b7f51e2277e58dace1fcbdd920
beyanon <fuyuki.yanagase@gmail.com>
2024-11-20 23:26:03 +0900

    feat : スクショ追加

--- _20241120_apply_font_to_all/lib/main.dart
+++ _20241120_apply_font_to_all/lib/main.dart
@@ -1,32 +1,64 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'Roboto', // デフォルトのフォントファミリーを設定
+        // カスタムフォントをデフォルトに設定
+        fontFamily: "noto",
       ),
-      home: MyHomePage(),
+      home: const FontExample(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class FontExample extends StatelessWidget {
+  const FontExample({Key? super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Custom Font Example'),
+        title: const Align(alignment: Alignment.center, child: Text("Apply Font to All")),
       ),
-      body: Center(
-        child: Text(
-          'Hello, Flutter!',
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "標準の中華フォントも直せます",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              "直認化細 (カスタムフォント)",
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 16),
+            DefaultTextStyle(
+              style: ThemeData.fallback().textTheme.bodyMedium!,
+              child: const Text(
+                "直認化細 (標準)",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "fontFamilyを指定しなくてもカスタムフォントが適用されています",
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 16),
+            const SizedBox(height: 40),
+          ],
         ),
       ),
     );
